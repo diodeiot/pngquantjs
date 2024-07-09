de05d1ff689f409c804716fe9b857ca908d928e3
kadir sevil <kadirsevil@MacBook-Pro.local>
2024-07-10 00:39:40 +0300

    fixed import issue

--- lib/index.ts
+++ lib/index.ts
@@ -1,31 +1,6 @@
-const addon = require("../index.node");
+//inspired from https://github.com/dubzzz/pure-rand/
 
-export type Result = {
-  palette: Buffer;
-  pixels: Buffer;
-  quality: number;
-};
+import * as pngquantjs from "./index-default";
+export default pngquantjs;
 
-export type Options = {
-  speed: number;
-  qMin: number;
-  qMax: number;
-  ditheringLevel: number;
-};
-
-export function quantize(
-  buffer: Buffer,
-  width: number,
-  height: number,
-  options?: Options
-): Result {
-  return addon.quantize(
-    buffer,
-    width,
-    height,
-    options?.speed ?? 4,
-    options?.qMin ?? 0,
-    options?.qMax ?? 100,
-    options?.ditheringLevel ?? 1.0
-  );
-}
+export * from "./index-default";
